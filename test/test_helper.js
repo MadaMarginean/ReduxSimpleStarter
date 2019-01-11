c401d16b7d304030e5972439245cf1cc034dfaef
Madalina Marginean <madalina.marginean@mcro.tech>
2019-01-11 12:44:29 +0200

    Test for App component

--- test/test_helper.js
+++ test/test_helper.js
@@ -9,7 +9,10 @@ import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import reducers from '../src/reducers';
 
-global.document = jsdom.jsdom('<!doctype html><html><body></body></html>');
+const { JSDOM } = jsdom;
+const { document } = (new JSDOM('\'<!doctype html><html><body></body></html>\'')).window;
+
+global.document = document;//jsdom.jsdom('<!doctype html><html><body></body></html>');
 global.window = global.document.defaultView;
 global.navigator = global.window.navigator;
 const $ = _$(window);
